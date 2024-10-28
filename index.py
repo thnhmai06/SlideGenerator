ba2d479750a8ced0fedd59a1f60fbe749895d56f
thnhmai06 <62001770+thnhmai06@users.noreply.github.com>
2024-10-28 09:00:04 +0700

    Reorganize files

--- index.py
+++ index.py
@@ -1,17 +1,16 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from src import *
 
-# Lấy UI
-try:
-    from gui import error, information, menu, progress
+try: #Thử Import module chứa UI
+    from src.ui import menu
 except: # UI chưa được chuyển thành code
-    import create_gui_script
-    create_gui_script.UIconverter() # Chuyển .ui -> .py
-    create_gui_script.QRCconverter() # Chuyển .qrc -> .py
+    convertUI.UIconverter() # Chuyển .ui -> .py
+    convertUI.QRCconverter() # Chuyển .qrc -> .py
 
     try: # Thử lại
-        from gui import error, information, menu, progress
-    except Exception as err:
+        from src.ui import menu
+    except Exception as err: # Báo lỗi
         print("ERR: Khong the chuyen file UI thanh code")
         print(err)
         print('\n')
@@ -20,52 +19,9 @@ except: # UI chưa được chuyển thành code
 app = QtWidgets.QApplication(sys.argv)
 window = QtWidgets.QMainWindow()
 
-#? Quy định trước HTML
-def successText(success, fails):
-    return f'<html><head/><body><p><span style=" font-size:10pt; font-weight:600;">Tạo Slide hoàn tất!</span></p><p><span style=" font-size:10pt;">Thành công: {success}/{success+fails}| Thất bại: {fails}/{success+fails}</span></p></body></html>'
-noPowerPointText = '<html><head/><body><p><span style=" font-size:10pt; font-weight:600;">Vui lòng cài đặt Microsoft PowerPoint (Office) <br/>để sử dụng phần mềm này!</span><br/></p></body></html>'
-
-#? Các hàm hiển thị UI
-def showMenu():
-    menu.Ui_menu().setupUi(window)
-    window.show()
-def showError(context):
-    #Tạo mới app, window khác
-    popup_app = QtWidgets.QApplication(sys.argv)
-    popup_window = QtWidgets.QMainWindow()
-    
-    # Thiết đặt: UI
-    ui = error.Ui_error_notice()
-    ui.setupUi(popup_window) # Xây dựng UI mẫu
-    ui.okbutton.clicked.connect(popup_app.quit) # Nút OK sẽ đóng cửa sổ
-
-    #Thiết đặt: window
-    popup_window.show() # Xây dựng (Hiển thị) window
-
-    #In ra lỗi
-    print(f"ERROR: {context}")
-
-    #Chạy hộp thoại
-    sys.exit(popup_app.exec_())
-def showInfo(context):
-    #Tạo mới app, window khác
-    popup_app = QtWidgets.QApplication(sys.argv)
-    popup_window = QtWidgets.QMainWindow()
-    
-    # Thiết đặt: UI
-    ui = information.Ui_information_notice()
-    ui.setupUi(popup_window) # Xây dựng UI mẫu
-    ui.label.setText(context) # Đặt nội dung của thông báo
-    ui.okbutton.clicked.connect(popup_app.quit) # Nút OK sẽ đóng cửa sổ
-    
-    #Thiết đặt: window
-    popup_window.show() # Xây dựng (Hiển thị) window
-
-    #Chạy hộp thoại
-    sys.exit(popup_app.exec_())
-
 if __name__ == "__main__":
-    showMenu()
+    # Hiển thị menu
+    showUI.Menu(window)
 
     #Chạy ứng dụng
     sys.exit(app.exec_())
---
+++ src/showUI.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from src.ui import error, information, progress, menu
+
+#? Các hàm hiển thị UI
+def Menu(window):
+    # Xây dựng UI mẫu
+    menu.Ui_menu().setupUi(window)
+    window.show()
+def Error(context):
+    #Tạo mới app, window khác
+    popup_app = QtWidgets.QApplication(sys.argv)
+    popup_window = QtWidgets.QMainWindow()
+    
+    # Thiết đặt: UI
+    ui = error.Ui_error_notice()
+    ui.setupUi(popup_window) # Xây dựng UI mẫu
+    ui.okbutton.clicked.connect(popup_app.quit) # Nút OK sẽ đóng cửa sổ
+
+    #Thiết đặt: window
+    popup_window.show() # Xây dựng (Hiển thị) window
+
+    #In ra lỗi
+    print(f"ERROR: {context}")
+
+    #Chạy hộp thoại
+    sys.exit(popup_app.exec_())
+def Info(context):
+    #Tạo mới app, window khác
+    popup_app = QtWidgets.QApplication(sys.argv)
+    popup_window = QtWidgets.QMainWindow()
+    
+    # Thiết đặt: UI
+    ui = information.Ui_information_notice()
+    ui.setupUi(popup_window) # Xây dựng UI mẫu
+    ui.label.setText(context) # Đặt nội dung của thông báo
+    ui.okbutton.clicked.connect(popup_app.quit) # Nút OK sẽ đóng cửa sổ
+    
+    #Thiết đặt: window
+    popup_window.show() # Xây dựng (Hiển thị) window
+
+    #Chạy hộp thoại
+    sys.exit(popup_app.exec_())
