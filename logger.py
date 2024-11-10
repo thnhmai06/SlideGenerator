b6f6aced9d35d86280b173b5e67b91db18c4602a
thnhmai06 <62001770+thnhmai06@users.noreply.github.com>
2024-11-10 20:23:05 +0700

    refactor: Remove unnecessary file logger.py on .

--- logger.py
+++
@@ -1,6 +0,0 @@
-import logging
-from ui import diaglogs
-
-def error(window_name: str, title: str, details: str) -> None:
-    diaglogs.error(window_name, title, details) # Show error dialog
-    logging.error(details) # Log error details
