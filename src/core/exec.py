c07b89a54621f38f8d16677d62daea73d41ff5a1
thnhmai06 <62001770+thnhmai06@users.noreply.github.com>
2024-11-28 22:24:47 +0700

    feat: update exec function to allow custom range for student processing

--- src/core/exec.py
+++ src/core/exec.py
@@ -1,10 +1,8 @@
 from typing import TYPE_CHECKING
-from translations import TRANS
 from globals import input
 
 if TYPE_CHECKING:
     # Anti-circular import
-    from src.ui.menu import Menu
     from src.ui.progress import Progress
 
 class LogLevel():
@@ -17,8 +15,7 @@ def per_processing(progress: "Progress", num: int):
     student = input.csv.get(num)
 
 
-def exec(progress: "Progress"):
+def exec(progress: "Progress", from_: int = 1, to_: int = input.csv.number_of_students):
     progress.add_log(__name__, __loglevel.info, "start")
-    for num in range(1, input.csv.number_of_students + 1): 
+    for num in range(from_, to_ + 1): 
         per_processing(progress, num)
-        
