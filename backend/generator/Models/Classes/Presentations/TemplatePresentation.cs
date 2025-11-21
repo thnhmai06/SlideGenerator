62fc302145a8b2467db8f886d757e7e44434d3a5
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-11-21 16:28:12 +0700

    feat: Thiết lập ASP.Net Server và thêm các Presentations class

---
+++ backend/generator/Models/Classes/Presentations/TemplatePresentation.cs
@@ -0,0 +1,34 @@
+﻿using DocumentFormat.OpenXml.Presentation;
+using generator.Models.Exceptions.Presentations;
+
+namespace generator.Models.Classes.Presentations
+{
+    public sealed class TemplatePresentation : Presentation
+    {
+        private const int FirstSlideIndex = 0;
+        private readonly string _mainSlideRelationshipId;
+
+        public TemplatePresentation(string filepath) : base(filepath, true)
+        {
+            var slideIds = GetSlideIdList().ChildElements;
+            if (slideIds.Count != 1)
+                throw new NotOnlySlidePresentationException(filepath, slideIds.Count);
+
+            var slideId = (SlideId)slideIds[FirstSlideIndex];
+            _mainSlideRelationshipId = slideId.RelationshipId?.Value ?? throw new NoRelationshipIdSlideException(filepath, FirstSlideIndex + 1);
+        }
+
+        public IEnumerable<Text> GetSlideText()
+        {
+            return GetSlideText(_mainSlideRelationshipId);
+        }
+        public IEnumerable<Shape> GetSlideShapes(bool mustFilledByImage = false)
+        {
+            return GetSlideShapes(_mainSlideRelationshipId, mustFilledByImage);
+        }
+        public IEnumerable<Picture> GetSlidePictures()
+        {
+            return GetSlidePictures(_mainSlideRelationshipId);
+        }
+    }
+}
