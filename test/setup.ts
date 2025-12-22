7ddc9e387c0cb8cb802184f1e044b9a950591452
Mai Thành <62001770+thnhmai06@users.noreply.github.com>
2025-12-23 04:43:22 +0700

    tests: cập nhật test cho fromtend

--- test/setup.ts
+++ test/setup.ts
@@ -1,1 +1,22 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+
+// Mock localStorage
+const localStorageMock = (function () {
+  let store: Record<string, string> = {};
+  return {
+    getItem: (key: string) => store[key] || null,
+    setItem: (key: string, value: string) => {
+      store[key] = value.toString();
+    },
+    removeItem: (key: string) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", {
+  value: localStorageMock,
+});
